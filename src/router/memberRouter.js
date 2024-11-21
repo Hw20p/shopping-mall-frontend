7f914a62dcd8045922ae52f9848d5912c49d2107
Hw20p <dlgusdn0129@naver.com>
2024-11-21 14:02:40 +0900

    9장 소셜 로그인

--- src/router/memberRouter.js
+++ src/router/memberRouter.js
@@ -1,10 +1,14 @@
 import { Suspense, lazy } from "react";
-const Loading = <div>Loading....</div>
-const Login = lazy(() => import("../pages/member/LoginPage"))
 
+const Loading = <div>Loading....</div>
+
+const Login = lazy(() => import("../pages/member/LoginPage"))
 
 const LogoutPage = lazy(() => import("../pages/member/LogoutPage"))
 
+const KakaoRedirect = lazy(() => import("../pages/member/KakaoRedirectPage"))
+
+const MemberModify = lazy(() => import("../pages/member/ModifyPage"))
 
 const memberRouter = () => {
 
@@ -16,7 +20,17 @@ const memberRouter = () => {
         {
             path: "logout",
             element: <Suspense fallback={Loading}><LogoutPage /></Suspense>,
-        }
+        },
+        {
+            path: "kakao",
+            element: <Suspense fallback={Loading}><KakaoRedirect /></Suspense>,
+        },
+        {
+            path: "modify",
+            element: <Suspense fallback={Loading}><MemberModify /></Suspense>,
+        },
+
+
 
     ]
 
--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import loginSlice from './slices/loginSlice'
 
 export default configureStore({
-  reducer: { 
-    "loginSlice": loginSlice
-  }
+    reducer: {
+        "loginSlice": loginSlice
+    }
 })
